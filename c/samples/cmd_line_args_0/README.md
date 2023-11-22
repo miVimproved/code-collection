c5c4d02d23204d3abb3c21ac08e37cdd3313dd44
mivimproved <143461207+miVimproved@users.noreply.github.com>
2023-11-22 13:39:34 -0800

    Added c/s/cmd_line_args_0, readme isn't done yet.

---
+++ c/samples/cmd_line_args_0/README.md
@@ -0,0 +1,42 @@
+# Command Line Argumens 0
+
+**I'm not done writing the readme file, but I have finished the code.**
+
+Command line arguments are very important in making programs.
+
+## Topics
+
+- Printing.
+
+## How to run
+
+- Linux:
+  - Run `make compile` to compile the program.
+  - Run `./cmdlineargs %s` replacing `%s` with the arguments you want to pass in.
+  - Run `make clean` to remove the build files.
+- Windows:
+  - TODO
+
+## Tinker Ideas
+
+- Hello.
+
+## Todo list
+
+### Known Issues
+
+- Windows has nothing yet lmao.
+
+### Setup
+
+- [X] Linux.
+  - [X] Makefile.
+  - [X] Compilers.
+    - [X] GCC.
+    - [X] Clang.
+
+- [ ] Windows.
+  - [ ] Batch File.
+  - [ ] Compilers.
+    - [ ] MSVC.
+    - [ ] GCC.
---
+++ c/samples/cmd_line_args_0/main.c
@@ -0,0 +1,45 @@
+/**
+ * @file main.c
+ * @author Vim
+ * @brief A simple example of printing integers in C.
+ * @version 0.1
+ */
+
+#include <stdio.h>
+
+/**
+ * @brief The function "main" is a reserved function in C.
+ * 		This function will always be the first function called in a program,
+ * 		it is the starting point.
+ * 
+ * @return int 
+ */
+
+/**
+ * @brief This main function takes in two parameters. The first one is the amount
+ * 		of arguments passed in, and the second one... I've rewritten this line many times,
+ * 		check README.md to understand these parameters.
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char** argv) {
+	// Check the arg count, and make sure at least one user argument
+	// was passed in. I don't need to do this here, however it teaches you
+	// that you can, and should, check the amount of arguments that are
+	// passed in when writing funcitons.
+	if (argc < 2) {
+		// perror is like printf but not.
+		perror("[Error] No user arguments passed in");
+		return -1;
+	}
+
+	// Print out all of the arguments
+	for (int i = 0; i < argc; i++) {
+		printf("%s\n", argv[i]);
+	}
+
+	// All good! Return 0.
+	return 0;
+}
